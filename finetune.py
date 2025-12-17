ea4cddf6a293a63b1359e83248bebca876ff1f6b
Holder <95602408+PaintingProgrammer@users.noreply.github.com>
2025-12-17 10:53:45 +0800

    Add placeholder finetune.py - actual file to be obtained from bigcode-project/starcoder repository

---
+++ finetune.py
@@ -0,0 +1,4 @@
+# Placeholder for finetune.py
+# The actual finetune.py file can be found in the bigcode-project/starcoder repository
+# Visit: https://github.com/bigcode-project/starcoder
+# Search for 'finetune.py' in the repository to locate the actual file.
